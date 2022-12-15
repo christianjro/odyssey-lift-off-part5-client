5b1b4cf15adb5fc919006a5d34799a207398c01e
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2022-12-14 17:16:19 -0800

    update to production uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://railway.app/project/11b6bbcc-4a7f-4c9c-b1c9-2a9620eca3fe/service/3cb5bb48-e3f4-4a80-b97a-1ebc8a2614d4",
+  uri: "odyssey-lift-off-part5-server-production-a7ac.up.railway.app",
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
